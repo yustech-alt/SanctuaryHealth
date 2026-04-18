f0e696f0db6ecbfe2db30a03a8767d240d9eb17c
fikkii <ajalafikayo@gmail.com>
2026-04-18 10:31:38 +0100

    Completed the Landing Page but adjustments can still be made later on...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
-import Dashboard from "./pages/dashboard";
 import LandingPage from "./pages/landing-page";
+
 import LoginPage from "./pages/authPages/LoginPage";
 import SignupPage from "./pages/authPages/SignupPage";
+
+import DashboardHome from "./pages/dashboardPages/DashboardHome";
+
 import AppLayout from "./ui/AppLayout";
 import AuthLayout from "./ui/layouts/AuthLayout";
+import DashboardLayout from "./ui/layouts/DashboardLayout";
 import { HeroUIProvider } from "@heroui/react";
 
 export default function App() {
@@ -15,10 +19,12 @@ export default function App() {
         <Routes>
           <Route path="/" element={<AppLayout />}>
             <Route index element={<LandingPage />} />
-            <Route path="dashboard" element={<Dashboard />} />
             <Route path="/auth" element={<AuthLayout />}>
               <Route path="login" element={<LoginPage />} />
               <Route path="signup" element={<SignupPage />} />
+            </Route>
+            <Route path="/dashboard" element={<DashboardLayout />}>
+              <Route index element={<DashboardHome />} />
             </Route>
           </Route>
         </Routes>
